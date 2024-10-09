10a38952d090d7388dd648083f6b3027113b32a4
TLZ <104867863+1010210@users.noreply.github.com>
2024-10-09 08:46:09 -0600

    Add files via upload

---
+++ netlify/functions/mongoDB.js
@@ -0,0 +1,7 @@
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+module.exports = client.connect();
